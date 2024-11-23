efdfd6a766cfc296b16fd70a0b84aacf4a4bc7ae
billalhossainjoy <billalhossain.bhj@gmail.com>
2024-11-23 13:06:25 +0600

    n

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       "/api": {
         target: process.env.VITE_REST_API+"/api",
         changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ""),
       },
     },
   },
