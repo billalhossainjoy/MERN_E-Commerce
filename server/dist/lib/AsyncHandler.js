d95f6202117362cab64988b6e19d048ea7c8498b
billalhossainjoy <billalhossain.bhj@gmail.com>
2024-11-23 11:29:20 +0600

    remove dist

--- server/dist/lib/AsyncHandler.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Asynchandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-exports.default = Asynchandler;
