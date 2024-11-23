825c9630a0ea503bb2c7961b7494e82abed3555c
billalhossainjoy <billalhossain.bhj@gmail.com>
2024-11-23 12:16:38 +0600

    update api link

--- client/src/lib/ApiClient.ts
+++ client/src/lib/ApiClient.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+
+console.log(import.meta.env.VITE_REST_API);
+
 const ApiClient = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_REST_API+"/api",
   withCredentials: true,
 });
 
